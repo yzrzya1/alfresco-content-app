0dd76cba732924209f5d4600c76a58a342a9e2c1
maurizio vitale <maurizio.vitale@alfresco.com>
2019-02-10 16:51:58 +0000

    Mandatory steps to enable the objects recognition extension by Maurizio Vitale

--- src/app/extensions.module.ts
+++ src/app/extensions.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AosExtensionModule } from '@alfresco/adf-office-services-ext';
+import { AlfrescoObjectRecognitionModule } from 'alfresco-objects-recognition';
 
 // Main entry point for external extensions only.
 // For any application-specific code use CoreExtensionsModule instead.
 
 @NgModule({
-  imports: [AosExtensionModule]
+  imports: [AosExtensionModule, AlfrescoObjectRecognitionModule]
 })
-export class AppExtensionsModule {}
+export class AppExtensionsModule { }
